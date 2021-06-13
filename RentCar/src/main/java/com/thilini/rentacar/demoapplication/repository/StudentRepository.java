7c81f6d316fb458419d20703af869a07c2ec225e
Thilini-Weerasinghe <thiliniweerasinghe09@gmail.com>
2021-06-14 02:42:43 +0530

    remove the file

--- RentCar/src/main/java/com/thilini/rentacar/demoapplication/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thilini.rentacar.demoapplication.repository;
-
-import com.thilini.rentacar.demoapplication.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
